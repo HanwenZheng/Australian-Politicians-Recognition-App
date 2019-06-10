2dd30a1a9a5393ea27f6289301d8f93ae9634543
HanwenZheng <alex.zhenghw@gmail.com>
2019-06-10 21:18:09 +1000

    Update image_mirror.py

--- Image_Preprocessing/image_mirror.py
+++ Image_Preprocessing/image_mirror.py
@@ -14,14 +14,14 @@ path = 'D:/Pycharm/projects/faces/test/'
 cate = [path + '/' + x for x in os.listdir(path) if os.path.isdir(path + '/' + x)]
 
 for idx, folder in enumerate(cate):
-    if idx == 0:
-        for im in glob.glob(folder + '/*.jpg'):
-            r = random()
-            if r > percent2im:
-                print('Not image_mirroring image: %s' % (im))
-            else:
-                print('Image_mirroring image: %s' % (im))
-                image = cv2.imread(im)
-                image = cv2.flip(image, 1)
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                io.imsave(im.strip('.jpg') + '_imageMirror.jpg', image)
+    #if idx == 0:
+    for im in glob.glob(folder + '/*.jpg'):
+        r = random()
+        if r > percent2im:
+            print('Not image_mirroring image: %s' % (im))
+        else:
+            print('Image_mirroring image: %s' % (im))
+            image = cv2.imread(im)
+            image = cv2.flip(image, 1)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            io.imsave(im.strip('.jpg') + '_imageMirror.jpg', image)
